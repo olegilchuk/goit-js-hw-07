c6637c1e476b9a2d1c48017cbf2221b1b7b6a247
olegilchuk <77920910+olegilchuk@users.noreply.github.com>
2021-07-07 19:24:23 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,5 +1,6 @@
+
 const inputEl = document.querySelector('#validation-input');
-inputEl.addEventListener('focus', focusInputEl)
+inputEl.addEventListener('blur', focusInputEl)
 function focusInputEl(event) {
     const inputLength = Number(event.target.dataset.length);
     const inputValue = event.target.value.length;
@@ -10,5 +11,4 @@ function focusInputEl(event) {
         inputEl.classList.remove('valid');
         inputEl.classList.add('invalid');
     }
-    console.log(inputLength === inputValue)
-}
+};
